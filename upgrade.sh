2e47d5f68f4f91c33f091c27ed2ea50c37969b35
Tyler Jang <tyler@trunk.io>
2023-03-14 16:58:00 -0700

    cleanup, comments

--- upgrade.sh
+++ upgrade.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 # trunk-ignore(shellcheck/SC2086): pass arguments directly as is
 upgrade_output=$(${TRUNK_PATH} upgrade --no-progress -n ${UPGRADE_ARGUMENTS} | sed -e 's/\x1b\[[0-9;]*m//g' | grep "upgrade" -A 500)
-new_cli_version=$(echo "${upgrade_output}" | grep "cli upgrade" | awk '{print $NF}')
 title_message="Upgrade trunk"
 
+new_cli_version=$(echo "${upgrade_output}" | grep "cli upgrade" | awk '{print $NF}')
 if [[ -n ${new_cli_version} ]]; then
   title_message="Upgrade trunk to ${new_cli_version}"
 fi
@@ -16,22 +16,18 @@ ${TRUNK_PATH} daemon shutdown
 git config --unset core.hooksPath
 rm -f .trunk/landing-state.json
 
+# Replace space indentation with bulleted list (including sub-bullets)
 # trunk-ignore(shellcheck/SC2001): more complicated sed parsing required
 formatted_output=$(echo "${upgrade_output}" | sed -e 's/^\(  \)\{0,1\}  /\1- /')
 
 # TODO: TYLER FIX URL
+# Generate markdown
 description=$(UPGRADE_CONTENTS="${formatted_output}" envsubst <upgrade_pr.md)
-
-# TODO: TYLER ATTEMPT TO CLEAN THIS UP
-# trunk-ignore(shellcheck/SC2129): Write multi-line value to output
 
 {
   echo "DESCRIPTION<<EOF"
   echo "${description}"
   echo "EOF"
 } >>"${GITHUB_OUTPUT}"
-# echo "DESCRIPTION<<EOF" >>"${GITHUB_OUTPUT}"
-# echo "${description}" >>"${GITHUB_OUTPUT}"
-# echo "EOF" >>"${GITHUB_OUTPUT}"
 
 echo "TITLE_MESSAGE=${title_message}" >>"${GITHUB_OUTPUT}"
