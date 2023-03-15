318e4f9dfdad46196debe3b323b16a316f29a07d
Tyler Jang <tyler@trunk.io>
2023-03-15 10:28:59 -0700

    add packages for testing and trigger check issue

--- upgrade.sh
+++ upgrade.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 # trunk-ignore(shellcheck/SC2086): pass arguments directly as is
+
 upgrade_output=$(${TRUNK_PATH} upgrade --no-progress -n ${UPGRADE_ARGUMENTS} | sed -e 's/\x1b\[[0-9;]*m//g' | grep "upgrade" -A 500)
 title_message="Upgrade trunk"
 
