d2a6b5e83154bf011f9d491f0222ddd0fe6c3623
Tyler Jang <tyler@trunk.io>
2023-03-16 01:12:14 -0700

    debug run

--- upgrade/upgrade.sh
+++ upgrade/upgrade.sh
@@ -2,30 +2,29 @@
 
 set -euo pipefail
 
-echo "about to run upgrade" # TODO: TYLER REMOVE
-
 # trunk-ignore(shellcheck/SC2086): pass arguments directly as is
-upgrade_output=$(${TRUNK_PATH} upgrade --no-progress -n ${UPGRADE_ARGUMENTS} | sed -e 's/\x1b\[[0-9;]*m//g' | grep "upgrade" -A 500)
+upgrade_output=$(${TRUNK_PATH} upgrade --no-progress -n ${UPGRADE_ARGUMENTS} | sed -e 's/\x1b\[[0-9;]*m//g')
+
+if [[ ${upgrade_output} == *"Already up to date"* ]]; then
+  echo "Already up to date."
+  exit 0
+fi
+
+trimmed_upgrade_output=$(echo "${upgrade_output}" | grep "upgrade" -A 500)
 title_message="Upgrade trunk"
 
-echo "done running upgrade" # TODO: TYLER REMOVE
-
-new_cli_version=$(echo "${upgrade_output}" | grep "cli upgrade" | awk '{print $NF}')
+new_cli_version=$(echo "${trimmed_upgrade_output}" | grep "cli upgrade" | awk '{print $NF}')
 if [[ -n ${new_cli_version} ]]; then
   title_message="Upgrade trunk to ${new_cli_version}"
 fi
-
-echo "about to shutdown" # TODO: TYLER REMOVE
 
 # Avoid triggering a git-hook during the pull request creation action, and avoid resetting git hook config via daemon
 ${TRUNK_PATH} daemon shutdown
 git config --unset core.hooksPath
 
-echo "done shutting down" # TODO: TYLER REMOVE
-
 # Replace space indentation with bulleted list (including sub-bullets)
 # trunk-ignore(shellcheck/SC2001): more complicated sed parsing required
-formatted_output=$(echo "${upgrade_output}" | sed -e 's/^\(  \)\{0,1\}  /\1- /')
+formatted_output=$(echo "${trimmed_upgrade_output}" | sed -e 's/^\(  \)\{0,1\}  /\1- /')
 
 # TODO: TYLER FIX URL
 # Generate markdown
