7be77f0f17e91a3d86a4af288d8e4a3ba7cd98d2
Tyler Jang <tyler@trunk.io>
2023-03-16 01:10:55 -0700

    add debug statements

--- upgrade/upgrade.sh
+++ upgrade/upgrade.sh
@@ -2,19 +2,26 @@
 
 set -euo pipefail
 
+echo "about to run upgrade" # TODO: TYLER REMOVE
+
 # trunk-ignore(shellcheck/SC2086): pass arguments directly as is
 upgrade_output=$(${TRUNK_PATH} upgrade --no-progress -n ${UPGRADE_ARGUMENTS} | sed -e 's/\x1b\[[0-9;]*m//g' | grep "upgrade" -A 500)
 title_message="Upgrade trunk"
+
+echo "done running upgrade" # TODO: TYLER REMOVE
 
 new_cli_version=$(echo "${upgrade_output}" | grep "cli upgrade" | awk '{print $NF}')
 if [[ -n ${new_cli_version} ]]; then
   title_message="Upgrade trunk to ${new_cli_version}"
 fi
 
+echo "about to shutdown" # TODO: TYLER REMOVE
+
 # Avoid triggering a git-hook during the pull request creation action, and avoid resetting git hook config via daemon
 ${TRUNK_PATH} daemon shutdown
 git config --unset core.hooksPath
-rm -f .trunk/landing-state.json
+
+echo "done shutting down" # TODO: TYLER REMOVE
 
 # Replace space indentation with bulleted list (including sub-bullets)
 # trunk-ignore(shellcheck/SC2001): more complicated sed parsing required
